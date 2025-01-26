b3e71bad0209f14b8e00eb4aad70b7bb20f64919
M4rcioOliveira <marciooliveiracosta@outlook.com.br>
2025-01-26 13:56:36 -0300

    first commit

---
+++ src/main/java/com/github/m4rciooliveira/controller/LoanController.java
@@ -0,0 +1,12 @@
+package com.github.m4rciooliveira.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/loan")
+public class LoanController {
+
+    
+
+}
